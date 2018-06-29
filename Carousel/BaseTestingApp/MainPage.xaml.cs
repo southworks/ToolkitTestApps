eecd38dd9bb705b3765a8ebee14f898ba47d5db8
Lucas Rojas <lucas.rojas@southworks.com>
2018-06-29 17:33:01 -0300

    Upload Carousel

---
+++ Carousel/BaseTestingApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BaseTestingApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public List<BitmapImage> images { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            images = new List<BitmapImage>();
+
+            for(int i = 0; i < 25; i++)
+            {
+                images.Add(new BitmapImage(new Uri("https://picsum.photos/200/200/?random", UriKind.Absolute)));
+            }
+
+            MyCarousel.ItemsSource = images;
+        }
+    }
+}
