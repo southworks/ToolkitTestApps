6197dcdb8e6e39f94916f7be4fce263524673b65
Lucas Rojas <lucas.rojas@southworks.com>
2018-06-29 17:46:10 -0300

    Upload HeaderedContent

---
+++ HeaderedContentControl/BaseTestingApp/Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseTestingApp
+{
+    public class Result
+    {
+        public string Link { get; set; }
+        public string Summary { get; set; }
+        public string Title { get; set; }
+
+    }
+
+}
